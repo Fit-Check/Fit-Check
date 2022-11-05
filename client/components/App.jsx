813321bc0322827e77a8034d77436b48def0120e
harvli <hl.138@hotmail.com>
2022-11-05 17:04:56 -0400

    implemented  wardrobe, fitcheck path, MVP completed Co-authored-by: Shay Sheller shaysheller@users.noreply.github.com Co-authored-by: Ruqayaah Sabitu ruqayaahh@users.noreply.github.com Co-authored-by: Sarah Moosa sbethm@users.noreply.github.com

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,37 +3,40 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Form from './Form.jsx';
 import Fitcheck from './Fitcheck.jsx';
-// import './stylesheets/styles.css';
+import Wardrobe from './Wardrobe.jsx';
+import '../styles.css';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/'>
-          <div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
             <div>
-              <Link to='/fitCheck'>
-                <button id='goFit'>Go to Fitcheck</button>
-              </Link>
+              <div>
+                <Link to='/fitCheck'>
+                  <button id='goFit'>Go to Fitcheck</button>
+                </Link>
+              </div>
+              <Form />
             </div>
-            <Form />
-          </div>
-        </Route>
-        <Route path='/fitCheck'>
-          <div>
+          </Route>
+          <Route path='/fitCheck'>
             <div>
-              <Link to='/'>
-                <button id='back'>Back</button>
-              </Link>
+              <div>
+                <Link to='/'>
+                  <button id='back'>Back</button>
+                </Link>
+              </div>
+              <Fitcheck />
             </div>
-            <Fitcheck />
-          </div>
-        </Route>
-      </Switch>
-    </Router>
+          </Route>
+        </Switch>
+      </Router>
+      <br></br>
+      <Wardrobe />
+    </>
   );
 }
-
-//check
 
 export default App;
--- client/components/Fitcheck.jsx
+++ client/components/Fitcheck.jsx
@@ -1,9 +1,95 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function Fitcheck() {
-  return <p>Hi</p>;
+  const [topsArr, setTopsArray] = useState([
+    'blue shirt',
+    'green sweater',
+    'white jacket',
+    'red hoodie',
+    'denim jacket',
+  ]);
+  const [bottomsArr, setBottomsArray] = useState([
+    'blue jeans',
+    'sweats pants',
+    'cargo pants',
+    'black jeans',
+    'denim shorts',
+  ]);
+
+  const [currTop, chooseTop] = useState('');
+  const [currBottom, chooseBottom] = useState('');
+  const [weather, setWeather] = useState('');
+
+  const generateOutfit = () => {
+    const randBotNum = Math.floor(Math.random() * bottomsArr.length);
+
+    const randTopNum = Math.floor(Math.random() * topsArr.length);
+
+    chooseTop(topsArr[randTopNum]);
+    chooseBottom(bottomsArr[randBotNum]);
+    return;
+  };
+
+  //   useEffect(() => {
+  //     console.log('useeffect');
+  //     // function fetchData() {
+  //     //   fetch('/api/')
+  //     //     .then((data) => data.json())
+  //     //     .then((result) => {
+  //     //       setTopsArray(result.tops);
+  //     //       setBottomsArray(result.bottoms);
+  //     //     });
+  //     // }
+  //     // fetchData();
+  //     generateOutfit();
+  //   }, []);
+
+  function onSubmit() {
+    // send post request to server containing state
+    console.log(name, clothingType, weather);
+    axios
+      .post('/', {
+        weather: weather, // Sunny, Rainy, Cold, Hot // req.body.name/weather
+      })
+      .then((response) => {
+        console.log(response);
+        setTopsArray(response.tops);
+        setBottomsArray(response.bottoms);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    generateOutfit();
+  }
+
+  return (
+    <div>
+      <h2>What is the weather like today?</h2>
+      <form>
+        <label></label>
+        <select
+          value={weather}
+          name='weather'
+          onChange={(e) => setWeather(e.target.value)}
+        >
+          <option value=''></option>
+          <option value='Sunny'>Sunny</option>
+          <option value='Rainy'>Rainy</option>
+          <option value='Cold'>Cold</option>
+          <option value='Hot'>Hot</option>
+        </select>
+      </form>
+      <h1>Your outfit for the day!</h1>
+      <p>{currTop}</p>
+      <p>{currBottom}</p>
+      <button onClick={generateOutfit}>yolo</button>
+    </div>
+  );
 }
+
+// [top1, top2, top3][(bottom1, bottom2, bottom3)];
 
 export default Fitcheck;
