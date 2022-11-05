192ef228dd4c8ee619dd8ea5dfd72205e9c0965b
Sarah <14sbethm@gmail.com>
2022-11-05 11:32:11 -0500

    Codebase Setup Co-authored-by: Shay Sheller <shaysheller@users.noreply.github.com> Co-authored-by: Ruqayaah Sabitu <ruqayaahh@users.noreply.github.com> Co-authored-by: Harvey Li <harvli@users.noreply.github.com>

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    mode: process.env.NODE_ENV,
+    entry: './client/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: 'Development',
+            template: './index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test:/\.s?css/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+            }
+        ]
+    },
+    devServer: {
+        static: {
+            publicPath: '/build',
+            directory: path.resolve(__dirname, 'build')
+        },
+        proxy: {
+            '/api': 'http://localhost:3000',
+        }
+    }
+}
