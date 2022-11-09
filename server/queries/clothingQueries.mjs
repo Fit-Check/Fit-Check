c7f5fafee6e758dfbe72fe8d7d9f625ceb55cd90
ks1009 <karenshi1009@gmail.com>
2022-11-08 21:04:35 -0500

    fixed the frontend

--- server/queries/clothingQueries.mjs
+++ server/queries/clothingQueries.mjs
@@ -1,21 +1,25 @@
 import db from '../models/clothingModels.mjs';
-import {sortClothes} from '../helper/sortClothes.mjs';
-
-
+import { sortClothes } from '../helper/sortClothes.mjs';
 
 const getAllClothes = async (queryInfo) => {
   try {
-    const clothes = await db.query('SELECT * FROM clothes WHERE clothes.user_id=$1', queryInfo);
+    const clothes = await db.query(
+      'SELECT * FROM clothes WHERE clothes.user_id=$1',
+      queryInfo
+    );
     const sortedClothes = sortClothes(clothes.rows);
     return sortedClothes;
-  } catch(error) {
+  } catch (error) {
     console.log(error);
   }
 };
 
 const getWeatherClothes = async (queryInfo) => {
-  try{
-    const clothes = await db.query('SELECT * FROM clothes WHERE user_id=$1 AND weather=$2', queryInfo);
+  try {
+    const clothes = await db.query(
+      'SELECT * FROM clothes WHERE user_id=$1 AND weather=$2',
+      queryInfo
+    );
     const sortedClothes = sortClothes(clothes.rows);
     return sortedClothes;
   } catch (error) {
@@ -25,8 +29,11 @@ const getWeatherClothes = async (queryInfo) => {
 
 const saveNewClothes = async (queryInfo) => {
   try {
-    const savedClothe = await db.query('INSERT INTO clothes (user_id, name, weather, type) VALUES ($1, $2, $3, $4) RETURNING *', queryInfo);
-    return savedClothe;
+    const savedClothes = await db.query(
+      'INSERT INTO clothes (user_id, name, weather, type) VALUES ($1, $2, $3, $4) RETURNING *',
+      queryInfo
+    );
+    return savedClothes;
   } catch (error) {
     console.log(error, 'error in getWeatherClothes under queries');
   }
