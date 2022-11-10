f71e477ef4326ec9c9fae5e27afc6c95d2770288
DoctorCodeine <aimeeqnguyen@outlook.com>
2022-11-09 21:44:30 -0600

    updated wardrobe/aded seasonalClothing file

---
+++ client/seasonalClothes/SpringClothes.js
@@ -0,0 +1,33 @@
+const data = [
+    {
+        id: 0,
+        image: "./clothingImgs/leggings1.webp",
+        title: "grey leggings",
+        desc: "lululemon leggings",
+        type: "pants"
+    },
+    {
+      id: 1,
+      image: './clothingImgs/sweater2.jpg', 
+      title: 'nirvana sweater',
+      desc: 'nirvana sweater'
+    },
+    // {
+    //     id: 2,
+    //     image:
+    //     title:
+    //     desc:
+    // },
+    // {
+    //     id: 3,
+    //     image:
+    //     title:
+    //     desc:
+    // },
+    // {
+    //     id: 4,
+    //     image:
+    //     title:
+    //     desc:
+    // },
+]
